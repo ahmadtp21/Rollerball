44a316c5e46e654444420db8a76e280bf48af273
Your Name <you@example.com>
2022-09-16 12:50:12 +0700

    Import project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public TextMeshProUGUI countText;
+	public GameObject gameOverPanel;
+
+	[SerializeField] private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+
+		count = 0;
+		SetCountText();
+		gameOverPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(h, 0.0f, v);
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 8) 
+		{
+            gameOverPanel.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	public string escapeScene;
+	public bool isEscapeForQuit = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isEscapeForQuit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                Debug.Log("Name Scene: " + escapeScene);
+                SceneManager.LoadScene(escapeScene);
+            }
+        }
+	}
+
+	public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
